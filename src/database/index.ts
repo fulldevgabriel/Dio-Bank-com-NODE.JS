2c0fa1fb3378b5377a0fb26fccb7153cff499aff
s4veiro <biel14441@gmail.com>
2024-09-12 12:37:33 -0300

    configuração tabela do DB

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,17 @@
 import { DataSource } from "typeorm"
 
 export const AppDataSource = new DataSource({
-    type: "sqlite",
-    database: "./src/databse/db.sqlite",
+    type: 'sqlite',
+    database: './src/database/absolutepath/db.sqlite',
     migrations: [
-        "./src/database/migrations"
+        "./src/database/migrations/*.ts"
     ],
-})
+  });
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source inicializado!");
+    })
+    .catch((error) => {
+        console.error(error);
+    })
