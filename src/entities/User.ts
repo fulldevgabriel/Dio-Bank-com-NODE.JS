da3ba8983166d443a756a489d899f9576c924f48
s4veiro <biel14441@gmail.com>
2024-10-03 15:44:08 -0300

    cirando os respositories

---
+++ src/entities/User.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import { randomUUID } from "crypto";
+
+@Entity('')
+export class User {
+    constructor(
+        name: string,
+        email: string,
+        password: string
+    ){
+        this.user_id = randomUUID()
+        this.name = name
+        this.email = email
+        this.password = password
+
+    }
+
+    @PrimaryColumn()
+    user_id: string
+
+    @Column({ nullable: false })
+    name: string
+
+    @Column({ nullable: false })
+
+    email: string
+
+    @Column({ nullable: false })
+
+    password: string
+
+}
