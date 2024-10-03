b5e0bc917edc5a18f462ad15d682b427bc1e9c33
s4veiro <biel14441@gmail.com>
2024-10-03 16:29:00 -0300

    finalizando testes repository

--- src/repositories/UserRepository.spec.ts
+++ src/repositories/UserRepository.spec.ts
@@ -17,11 +17,14 @@ describe("UserRespository", () => {
     }
 
     beforeAll(async() => {
-        managerMock = await getMockEntityManager({})
+        managerMock = await getMockEntityManager({
+            saveReturn: mockUser
+        })
         userRepository = new UserRepository(managerMock as EntityManager)
     })
     it("Deve cadastrar um novo usuario no banco de dados", async() => {
-        await userRepository.createUser(mockUser)
+        const response = await userRepository.createUser(mockUser)
         expect(managerMock.save).toHaveBeenCalled()
+        expect(response).toMatchObject(mockUser)
     })
 })
--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -15,4 +15,13 @@ export class UserRepository {
         return this.manager.save(user)
 
     }
+
+    getUser = async (userId: string):Promise<User | null> => {
+        return this.manager.findOne(User, {
+            where: {
+                user_id: userId
+            }
+        })
+
+    }
 }
